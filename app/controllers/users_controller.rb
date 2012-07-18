b80077488c75bbf0ed03455128186610de7dfe2a
Aleksi Hentila <aleksi.hentila@gmail.com>
2012-07-18 14:38:06 +0300

    front-page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,8 +10,12 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
-      redirect_to root_path
-      flash[:notice] = "Esimies luotu!"
+      if @user.esimies
+          flash.now[:notice] = "Esimies luotu!"
+        else
+          flash.now[:notice] = "Myyja luotu!"
+      end
+      redirect_to new_user_path
       else
         flash[:error] = "Kayttajalomakkeessa oli virhe!"
         redirect_to root_path
